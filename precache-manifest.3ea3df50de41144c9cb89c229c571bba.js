7e8e505a0fd305bf5428527797c1e4491f037c24
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2021-07-12 11:22:30 -0400

    Updates

---
+++ precache-manifest.3ea3df50de41144c9cb89c229c571bba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7872acb8fb74ac820bb",
+    "url": "/ReactPortfolio/static/css/main.f3a332b9.chunk.css"
+  },
+  {
+    "revision": "f7872acb8fb74ac820bb",
+    "url": "/ReactPortfolio/static/js/main.f7872acb.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/ReactPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "aa22ff2d541828390b91",
+    "url": "/ReactPortfolio/static/js/runtime~main.aa22ff2d.js"
+  },
+  {
+    "revision": "b02205b6992d88253f6ec1afac0991d2",
+    "url": "/ReactPortfolio/index.html"
+  }
+];
