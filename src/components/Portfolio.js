643851e021298f1079d281896085face14501aef
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2021-08-19 16:25:05 -0400

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,7 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <a href={item.url}>
-                    <div className="item-wrap">
+                    <div className="item-wrap" target="_blank">
                       <img src={`${item.imgurl}`} className="item-img"/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
